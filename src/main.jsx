e50bf930591a6ad5fdcac12a2e033fc74c6eb84b
오지환 <ovitwisdom@gmail.com>
2025-06-04 01:41:46 +0900

    리엑트 세팅 중2

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import '../assets/scss/common-ui.scss';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<App />
+	</StrictMode>,
 )
